cc57d4e26381094d83d10e7bae4efc205d991106
Pookage <pookagehayes@gmail.com>
2018-06-03 16:02:22 +0100

    got the avatar looking at the cursor and register when movement keys are pressed

---
+++ Assets/Scripts/CursorWatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorWatcher : MonoBehaviour {
+
+	Transform avatar;
+
+	void Start(){
+		avatar = this.gameObject.transform.GetChild(0);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 v3T = Input.mousePosition;
+        v3T.z = Mathf.Abs(Camera.main.transform.position.y - avatar.transform.position.y);
+        v3T = Camera.main.ScreenToWorldPoint(v3T);
+        avatar.transform.LookAt(v3T);
+	}
+}
---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour {
+	
+	public KeyCode forward;
+	public KeyCode left;
+	public KeyCode backward;
+	public KeyCode right;
+
+	void Start(){
+		forward  = KeyCode.W;
+		left     = KeyCode.A;
+		backward = KeyCode.S;
+		right    = KeyCode.D;
+	}
+
+	void Update () {
+
+		if(Input.GetKeyDown(forward)){
+			Debug.Log("forward pressed");
+		}
+
+		if(Input.GetKeyDown(left)){
+			Debug.Log("left pressed");
+		}
+
+		if(Input.GetKeyDown(backward)){
+			Debug.Log("backward pressed");
+		}
+
+		if(Input.GetKeyDown(right)){
+			Debug.Log("right pressed");
+		}
+	
+	}
+}
