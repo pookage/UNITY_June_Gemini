7fa28508d3c157a7e0057df58ccd95625ed7d049
Pookage <pookagehayes@gmail.com>
2018-06-04 18:46:54 +0100

    added some smoothing to the avatar movement

--- Assets/Scripts/KeyboardInput.cs
+++ Assets/Scripts/KeyboardInput.cs
@@ -4,42 +4,41 @@ using UnityEngine;
 
 public class KeyboardInput : MonoBehaviour {
 	
-	public KeyCode forward;
-	public KeyCode left;
-	public KeyCode backward;
-	public KeyCode right;
-	public float speed = 10.0f;
+	public static KeyCode forward  = KeyCode.W;
+	public static KeyCode left     = KeyCode.A;
+	public static KeyCode backward = KeyCode.S;
+	public static KeyCode right    = KeyCode.D;
+	public static float speed      = 10.0f;
 
 	GameObject avatar;
+	Vector3 velocity = Vector3.zero;
+	float smoothTime = speed / 20;
 
 	void Start(){
-		forward  = KeyCode.W;
-		left     = KeyCode.A;
-		backward = KeyCode.S;
-		right    = KeyCode.D;
 		avatar   = this.gameObject;
-	}
+	}//Start
 
 	void Update () {
 
 		Vector2 direction = new Vector2(0, 0);
 
 		if(Input.GetKey(forward)){
-			direction.y  = direction.y + (speed * Time.deltaTime);
+			direction.y  = direction.y + speed;
 		} 
 
 		if(Input.GetKey(left)){
-			direction.x = direction.x + (-speed * Time.deltaTime);
+			direction.x = direction.x  - speed;
 		}
 
 		if(Input.GetKey(backward)){
-			direction.y = direction.y + (-speed * Time.deltaTime);
+			direction.y = direction.y  - speed;
 		}
 
 		if(Input.GetKey(right)){
-			direction.x = direction.x + (speed * Time.deltaTime);
+			direction.x = direction.x + speed;
 		}
 	
-		avatar.transform.Translate(direction.x, 0, direction.y);
-	}
+		Vector3 targetPosition    = avatar.transform.position + new Vector3(direction.x, 0, direction.y);
+		avatar.transform.position = Vector3.SmoothDamp(avatar.transform.position, targetPosition, ref velocity, smoothTime);
+	}//Update
 }
